0dcda69c48fd13f5830f2f70fbdd4cce777f122a
unknown <thalesalves758@gmail.com>
2022-04-05 22:21:14 -0300

    Solicita ao usuário o número de cartas e suas restrições

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+window.onload = handleLoad;
+
+let cardsNumber;
+
+function handleLoad() {
+    cardsNumber = Number(prompt("Com quantas cartas você deseja jogar?"));
+    let isInvalidNumber = isNaN(cardsNumber) || cardsNumber % 2 !== 0 || (cardsNumber < 4 || cardsNumber > 14);
+    
+    while(isInvalidNumber) {
+        alert("Número de cartas inválido! Insira novamente na próxima caixa de diálogo.");
+
+        cardsNumber = Number(prompt("Com quantas cartas você deseja jogar?"));
+        isInvalidNumber = isNaN(cardsNumber) || cardsNumber % 2 !== 0 || (cardsNumber < 4 || cardsNumber > 14);
+    }
+}
